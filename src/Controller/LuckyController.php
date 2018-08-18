b51a52e8a2c4e6e9dea471b8e53448664c22c9d7
rahrindra <rahrindra@gmail.com>
2018-08-18 17:14:14 +0300

    route localisé

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -9,13 +9,17 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
     /**
-     * @Route("/lucky/number")
+     * @Route({
+     *     "fr": "/nombre-chancheux",
+     *     "en": "/lucky-number"
+     * }, name="lucky_number")
      *
      * @return Response
      */
